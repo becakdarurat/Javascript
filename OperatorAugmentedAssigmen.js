e910df443ddfeb20904dbcc7e33c1da0fa0e7999
becakdarurat <becakdarurat>
2024-10-09 23:42:08 +0700

    "i learn short cut to Operator Augmented ASsigment"

---
+++ OperatorAugmentedAssigmen.js
@@ -0,0 +1,22 @@
+// Augmented Assigemnet / mempersingkat Aritmatika
+let angka = 3;
+
+// tambah
+angka += 3;
+console.log(angka);
+
+// Kurang
+angka -= 3;
+console.log(angka);
+
+// Kali
+angka *= 3;
+console.log(angka);
+
+// Bagi
+angka /= 2;
+console.log(angka);
+
+// sisa bagi
+angka %= 3;
+console.log(angka);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Javascript
 materi dari pelajaran udemy, pak eko kurniawan khannedy
+
+gunakan quoka.js untuk melihat result / hasil dari setiap javascript yang saya buat
