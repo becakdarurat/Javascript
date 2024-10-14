11e4f9af33d8d1a111e626d19e225d6f648f5748
becakdarurat <becakdarurat>
2024-10-14 20:27:59 +0700

    "Belajar Expression if else, Object dan Pop up"

--- Object.js
+++ Object.js
@@ -1,2 +1,39 @@
 // Deklarasi Object kosong
 let Person = {};
+
+// Menambah Object
+Person["nama"] = "Sofyan Tanjung";
+Person["Alamat"] = "Sibolga";
+Person["country"] = "Indonesia";
+Person["Usia"] = 23;
+
+// Menghapus Object
+delete Person["country"];
+console.table(Person);
+
+// Membuat Object dengan property
+const a = {
+    firstName : "Sofyan",
+    lastName : "Tanjung",
+    fullName : "Sofyan Tanjung"
+};
+
+console.table(a);
+
+const b = {
+    "ejaan huruf" : "Belajar lagi"
+}
+console.table(`Abjad Huruf ${b["ejaan huruf"]}`);
+console.table(b);
+
+// Mengaksess Property Object
+const car = {
+    warna : "Kuning",
+    merk : "Mercedez",
+    turbo : "aktif",
+};
+
+console.info(`warna ${car.warna}`);
+console.info(`warna ${car.merk}`);
+console.info(`warna ${car.turbo}`);
+
---
+++ ifExpression.js
@@ -0,0 +1,23 @@
+// Kode if Expression
+const examValue = 90;
+// with if 
+if ( examValue > 80 ) {
+    console.log(`Good Job`);
+}
+// with else if
+if ( examValue < 80 ) {
+    console.log(`Good Job`);
+} else if ( examValue == 100 ) {
+    console.log(`Nilainya sama`);
+}
+  else if ( examValue == 90 ) {
+    console.log(`Nilainya sama`);
+} else {
+    console.log(`Try Again Next Year`);
+}
+// with else
+if ( examValue < 80 ) {
+    console.log(`Good Job`);
+} else {
+    console.log(`Try Again Next Year`);
+}
