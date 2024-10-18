5655308e6338c8a8e84945defe315d730481b531
becakdarurat <becakdarurat>
2024-10-18 21:11:48 +0700

    "belajar For loop dan logika booelean falsy dan thruty"

---
+++ ForLoop.js
@@ -0,0 +1,12 @@
+// For Perulangan
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+// Perulangan dengan kondisi
+let counter = 1;
+for ( ; counter <= 5;) {
+    console.log(`Perulangan ke ${counter}`);
+    counter++;
+}
+console.log(counter);
--- LogikaBoolean.js
+++ LogikaBoolean.js
@@ -1,10 +1,26 @@
-// Operator Logika non Boolean
+// Operator Logika non Boolean FALSEY
 console.log(`Hello` || '');
 console.log("" || []);
 console.log(`0` || `NOL`);
 console.log(`0` || "NOL");
 console.log(null || `NULL`);
 console.log(undefined || `UNDEFINED`);
+
+// Operator Logika non Boolean THRUTY
+console.log(`Hello` && '');
+console.log("" && []);
+console.log(`0` && `NOL`);
+console.log(`0` && "NOL");
+console.log(null && `NULL`);
+console.log(undefined && `UNDEFINED`);
+
+// Contoh penggunaannya
+const person = {
+    firstName : "",
+    lastName : "Sibolga",
+};
+const nama = person.firstName || person.lastName;
+console.log(nama);
 
 
 
